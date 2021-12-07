d5af4e5d8fb73e2ade315b14ea591ae4f2e0215b
hangeulisbest <wj100213@nate.com>
2021-12-08 01:56:23 +0900

    DaoFactory Test추가

--- src/src/test/java/com/springstudy/demo/ch1/IoC/DaoFactoryTest.java
+++ src/src/test/java/com/springstudy/demo/ch1/IoC/DaoFactoryTest.java
@@ -18,4 +18,23 @@ class DaoFactoryTest {
         UserDao dao = context.getBean("userDao", UserDao.class);
         assertEquals(dao.get("1").getName(),"hello");
     }
+
+    @Test
+    public void daoFactory에서_생성한_오브젝트는_동일하지_않다(){
+        DaoFactory daoFactory = new DaoFactory();
+        UserDao dao1 = daoFactory.userDao();
+        UserDao dao2 = daoFactory.userDao();
+
+        assertEquals(dao1==dao2,false);
+
+    }
+
+    @Test
+    public void ApplicationContext에서_가져온_userDao는_싱글톤이다(){
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao1 = context.getBean("userDao", UserDao.class);
+        UserDao dao2 = context.getBean("userDao", UserDao.class);
+
+        assertEquals(dao1==dao2,true);
+    }
 }
